7f204e408f8b9bdf2a356a4be4de8e77eb06bf4a
dakotek <davidsanchezdawdam@gmail.com>
2023-10-09 10:03:54 +0200

    estructura inicial

--- app/src/main/java/com/example/artspace/MainActivity.kt
+++ app/src/main/java/com/example/artspace/MainActivity.kt
@@ -3,13 +3,21 @@ package com.example.artspace
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.artspace.ui.theme.ArtSpaceTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,27 +28,144 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    color = Color.Black
                 ) {
-                    Greeting("guille")
+                    mainScreen()
                 }
             }
         }
     }
 }
 
+data class Cuadro(
+    val imageResource: Int,
+    val title: String,
+    val artist: String,
+    val year: String
+)
+
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun mainScreen() {
+    val imageResources = listOf(
+        Cuadro(
+            imageResource = R.drawable.jardindelicias,
+            title = stringResource(R.string.title_jardindelicias),
+            artist = stringResource(R.string.artist_jardindelicias),
+            year = stringResource(R.string.year_jardindelicias)
+        ),
+        Cuadro(
+            imageResource = R.drawable.balsamedusa,
+            title = stringResource(R.string.title_balsamedusa),
+            artist = stringResource(R.string.artist_balsamedusa),
+            year = stringResource(R.string.year_balsamedusa)
+        ),
+        Cuadro(
+            imageResource = R.drawable.entierroornans,
+            title = stringResource(R.string.title_entierroornans),
+            artist = stringResource(R.string.artist_entierroornans),
+            year = stringResource(R.string.year_entierroornans)
+        ),
+        Cuadro(
+            imageResource = R.drawable.nocheestrellada,
+            title = stringResource(R.string.title_nocheestrellada),
+            artist = stringResource(R.string.artist_nocheestrellada),
+            year = stringResource(R.string.year_nocheestrellada)
+        ),
+        Cuadro(
+            imageResource = R.drawable.jugadorescartas,
+            title = stringResource(R.string.title_jugadorescartas),
+            artist = stringResource(R.string.artist_jugadorescartas),
+            year = stringResource(R.string.year_jugadorescartas)
+        ),
+        Cuadro(
+            imageResource = R.drawable.nighthawks,
+            title = stringResource(R.string.title_nighthawks),
+            artist = stringResource(R.string.artist_nighthawks),
+            year = stringResource(R.string.year_nighthawks)
+        ),
     )
+
+    var currentIndex by remember { mutableStateOf(0) }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.SpaceBetween,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painterResource(id = imageResources[currentIndex].imageResource),
+            contentDescription = null,
+            contentScale = ContentScale.FillBounds,
+            modifier = Modifier
+                .fillMaxWidth()
+                .fillMaxHeight(0.6f)
+        )
+        Text(
+            text = imageResources[currentIndex].title,
+            color = Color.White,
+            textAlign = TextAlign.Center,
+            fontWeight = FontWeight.Bold,
+            fontSize = 24.sp,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp)
+        )
+        Text(
+            text = imageResources[currentIndex].artist + " - " + imageResources[currentIndex].year,
+            color = Color.White,
+            textAlign = TextAlign.Center,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp)
+        )
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Button(
+                onClick = {
+                    if (currentIndex > 0) {
+                        currentIndex--
+                    } else {
+                        currentIndex = imageResources.size - 1
+                    }
+                },
+                modifier = Modifier
+                    .padding(16.dp)
+                    .fillMaxWidth()
+            ) {
+                Text(text = stringResource(R.string.btn1))
+            }
+        }
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Button(
+                onClick = {
+                    if (currentIndex < imageResources.size - 1) {
+                        currentIndex++
+                    } else {
+                        currentIndex = 0
+                    }
+                },
+                modifier = Modifier
+                    .padding(16.dp)
+                    .fillMaxWidth()
+            ) {
+                Text(text = stringResource(R.string.btn2))
+            }
+        }
+    }
 }
+
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     ArtSpaceTheme {
-        Greeting("Android")
+        mainScreen()
     }
 }
